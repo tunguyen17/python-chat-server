26a865a4de62431a63432bf1c5c98a42246a1b1f
TuNguyen <tanguyen17@wabash.edu>
2016-10-07 16:06:00 -0400

    update user interface

--- client.py
+++ client.py
@@ -168,7 +168,7 @@ class Client(object):
                 usr = raw_input("Username: ")
             self.client.send(usr)
             data = self.client.recv(self.size)
-            if data!= '!#@useraccepted**':
+            if data == '!#@useraccepted**':
                 data = '\33[92mUser accepted\33[0m'
             print data
 
